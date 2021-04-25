9ad96c7ac1bac00719046745b59c66938720503a
abdulbakisurajtest <surajabdulbaki19@gmail.com>
2021-04-25 17:31:18 +0100

    update readme

--- conn.php
+++ conn.php
@@ -4,28 +4,10 @@
 session_start();
 
 // initialize connection variables
-$http = $_SERVER['HTTP_HOST'];
-$host = "";
-$user = "";
-$pass = "";
-$dbname = "";
-
-// environment is localhost
-if($http === 'localhost' || $http === '127.0.0.1'):
-	$host = 'localhost';
-	$username = 'root';
-	$password = '';
-	$dbname = 'zuri_task_apr_23_2021';
-
-//	environment is online
-else:
-	//Get Heroku ClearDB connection information
-	$cleardb_url = parse_url("mysql://bcc99456d62e26:de06b3f3@us-cdbr-east-03.cleardb.com/heroku_7296d17177d64f8?reconnect=true");
-	$host = $cleardb_url["host"];
-	$username = $cleardb_url["user"];
-	$password = $cleardb_url["pass"];
-	$dbname = substr($cleardb_url["path"],1);
-endif;
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'zuri_task_apr_23_2021';
 
 try
 {
