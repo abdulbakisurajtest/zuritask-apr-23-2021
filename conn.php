9dc7b0f5fbfe38c7aee421c3ba72e29af6bb8e07
abdulbakisurajtest <surajabdulbaki19@gmail.com>
2021-04-25 07:42:20 +0100

    added connection to heroku

--- conn.php
+++ conn.php
@@ -3,11 +3,29 @@
 // start session
 session_start();
 
-// instantiate database variables
-$host = 'localhost';
-$dbname = 'zuri_task_apr_23_2021';
-$username = 'root';
-$password = '';
+// initialize connection variables
+$http = $_SERVER['HTTP_HOST'];
+$host = "";
+$user = "";
+$pass = "";
+$dbname = "";
+
+// environment is localhost
+if($http === 'localhost' || $http === '127.0.0.1'):
+	$host = 'localhost';
+	$username = 'root';
+	$password = '';
+	$dbname = 'zuri_task_apr_23_2021';
+
+//	environment is online
+else:
+	//Get Heroku ClearDB connection information
+	$cleardb_url = parse_url("mysql://bcc99456d62e26:de06b3f3@us-cdbr-east-03.cleardb.com/heroku_7296d17177d64f8?reconnect=true");
+	$host = $cleardb_url["host"];
+	$username = $cleardb_url["user"];
+	$password = $cleardb_url["pass"];
+	$dbname = substr($cleardb_url["path"],1);
+endif;
 
 try
 {
